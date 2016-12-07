8ea69d457773ce1adea38663cebcad7a55587082
tiiarautavesi <tiia.rautavesi@metropolia.fi>
2016-12-07 11:59:23 +0200

    uutta paskaa

---
+++ Kimppakyyti/confirm.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="icon" href="img/LOGO_sqr.png">
+        <link rel="stylesheet" href="css/tyyli.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="css/haku.css" type="text/css" media="screen">
+        <title>KIMPPAKYYTI</title> 
+    </head>
+    
+    <body>
+        <header>
+            <aside><img src="img/purilainen2.png"></aside>
+            <h2>Rekisteröityminen</h2>
+            <nav>
+                <ul>
+                    <li>Kyytitarjoukset</li>
+		    		<li>Kyytipyynnöt</li>
+		    		<li><a href="logIn.php">Log in</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+<?php
+    //Lomakkeen syöttötiedot $data[] taulukossa
+    $data = $_POST['data'];
+    //Laitetaan syötetyt tiedot sessioon jemmaan, jotta voidaan palata muuttamaan
+    $_SESSION['givenData'] = serialize($data);
+
+    //nimi ja email oikein? tarkistus palvelimella
+    if(filter_var($data['email'], FILTER_VALIDATE_EMAIL)) { //valmis php funktio
+        if(preg_match("/^[a-öA-Ö]*$/",$data['lname'])) { //sallitaan kirjaimia ja välilyöntejä
+            //* on "useita" ja ^ on "täytyy alkaa"
+            
+                echo '<div class="tiedot">';
+                    echo 'Nimi: '.$data['fname'].' '.$data['lname'] ."<br>";
+                    echo 'Sähköposti: '.$data['email'] ."<br>";
+                    echo 'Puhelinnumero: '.$data['phone'] ."<br>";
+                    echo 'Syntymäaika: '.$data['bday'] ."<br>";
+                    echo 'Sukupuoli: '.$data['sex'] ."<br>";
+                    echo '</div>';
+                    echo '<a href="saveUser.php">Tallenna</a>';
+        }else{
+            echo("<p>Sukunimessä sallittu vain kirjaimet ja välilyönnit!<br/>".$data['lname']."</p>");
+        }
+        echo '<a href="register.php">Takaisin</a>';
+    }else{
+            echo("<p>Sähköposti muodossa example@example.com<br>".$data['email']."</p>");
+    }
+?>
+        </section>
+    </body>
+</html>
---
+++ Kimppakyyti/luo-pyynto.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<!--tee rekisteröitymislomake, jonka suvi jo teki hahaa-->
+
+<html>
+    <head>
+    <meta charset="utf-8">
+        <link rel="icon" href="img/LOGO_sqr.png">
+        <link rel="stylesheet" href="css/tyyli.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="css/haku.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" media="screen">
+        <title>KIMPPAKYYTI</title> 
+        
+
+        
+    </head>
+    <body>
+        <header>
+            <nav>
+                    <ul>
+                        <?php
+                                include_once("includes/loggedin_header.php");
+                        ?>
+
+                    </ul>
+                </nav>
+        </header>
+    
+        <main>
+            
+            <section>
+                <div class="form-style-6">
+				    <h1>Luo kyytipyyntö</h1>
+                    <br>
+						<form method="POST" action="saveUser.php">
+							<input type="text" name="givenOdeparture" placeholder="Lähtöpaikka" required/>
+							<input type="text" name="givenOarrival" placeholder="Saapumispaikka" required/>
+                            <input type="date" name="givenOdate" placeholder="Päivämäärä pv.kk.vvvv" required/>
+                            <input type="time" name="givenOtime" placeholder="Lähtöaika" />
+							<input type="text" name="givenOseats" placeholder="Tarvittavien paikkojen määrä" required/>
+							<input type="submit" name="save" value="Luo pyyntö" />
+                            <input type="reset" value="Nollaa tiedot" />
+						</form>
+                    <script>
+                        var salasana = document.querySelector('input[name="givenPassword"]');
+                        var varmistus = document.querySelector('input[name="givenPassword2"]');
+                        console.log(varmistus);
+                        var fillPattern = function(){
+                                varmistus.pattern = salasana.value;
+                            }
+                        salasana.addEventListener('keyup', fillPattern);
+                    </script>
+				</div>
+            
+            
+            </section>
+        
+        
+        
+        </main>
+    
+    
+    
+    
+    
+    
+    </body>
+    <footer><center> <i> © Kimppakyyti // 2016</i></center></footer>
+</html>
